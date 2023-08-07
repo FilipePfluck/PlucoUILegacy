a39417a5badcb03878b5c699c3d3b74d51f5e3a9
Filipe Pfluck <filipepfluckdev@gmail.com>
2023-08-07 18:32:12 -0300

    adding mouse usage tracking

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,8 +1,41 @@
 'use client'
 
 import { ThemeProvider } from 'next-themes'
-import { ReactNode } from 'react'
+import { ReactNode, useEffect } from 'react'
 
 export const Providers = ({ children }: { children: ReactNode }) => {
+  useEffect(() => {
+    document.body.classList.add('using-mouse')
+    document.body.addEventListener('mousedown', function () {
+      document.body.classList.add('using-mouse')
+    })
+
+    const keys = [
+      'Tab',
+      'ArrowDown',
+      'ArrowUp',
+      'ArrowLeft',
+      'ArrowRight',
+      'Space',
+      'Enter',
+      'Escape',
+    ]
+
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (keys.includes(e.key)) {
+        document.body.classList.remove('using-mouse')
+      }
+    }
+
+    // Re-enable focus styling when a key is pressed
+    document.body.addEventListener('keydown', onKeyDown)
+    return () => {
+      document.body.removeEventListener('keydown', onKeyDown)
+      document.body.removeEventListener('mousedown', function () {
+        document.body.classList.add('using-mouse')
+      })
+    }
+  }, [])
+
   return <ThemeProvider attribute="class">{children}</ThemeProvider>
 }
