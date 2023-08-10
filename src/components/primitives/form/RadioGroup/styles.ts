f169bf9e46004db6624a39ffe7e9694dcfbdd06f
Filipe Pfluck <filipepfluckdev@gmail.com>
2023-08-10 18:14:56 -0300

    radio group

---
+++ src/components/primitives/form/RadioGroup/styles.ts
@@ -0,0 +1,58 @@
+import { cva } from '@/styled-system/css'
+import { styled } from '@/styled-system/jsx'
+import * as RadioGroup from '@radix-ui/react-radio-group'
+
+export const Root = styled(
+  RadioGroup.Root,
+  cva({
+    base: {
+      display: 'flex',
+      flexDir: 'column',
+      gap: '2',
+    },
+  }),
+)
+
+export const RadioItem = styled(
+  RadioGroup.Item,
+  cva({
+    base: {
+      display: 'flex',
+      center: 'flex',
+      bg: { base: 'mauve.3', _hover: 'purple.3' },
+      borderColor: { base: 'mauve.7', _checked: 'purple.8' },
+      borderStyle: 'solid',
+      borderWidth: '2px',
+      transition: '0.2s border-color',
+      w: '6',
+      h: '6',
+      rounded: 'full',
+      boxShadow: 'sm',
+      ring: '2px',
+    },
+  }),
+)
+
+export const RadioIndicator = styled(
+  RadioGroup.Indicator,
+  cva({
+    base: {
+      h: '3',
+      w: '3',
+      rounded: 'full',
+      bg: 'purple.8',
+      animation: 'check',
+      animationDuration: '0.1s',
+    },
+  }),
+)
+
+export const Label = styled(
+  'label',
+  cva({
+    base: {
+      color: 'mauve.12',
+      fontSize: '14px',
+    },
+  }),
+)
